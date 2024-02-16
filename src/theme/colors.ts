6f713a6ddbaee445359b24915eb26f569023e261
Saadi-SP-372 <119863927+Saadi-SP-372@users.noreply.github.com>
2024-02-16 18:00:15 +0100

    setting theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,13 @@
+const colors = {
+  white: "#FFFFFF",
+  black: "#000000",
+  dark: "#2D2D33",
+  grey: "#D0D4DA",
+  lightGrey: "#E3E3E3",
+  greySuit: "#BCBECB",
+  burntUmber: "#8C2727",
+  silverChalice: "#AAAAAA",
+  darkBlue: "#2F4368",
+};
+
+export default colors;
---
+++ src/theme/spacing.ts
@@ -0,0 +1,11 @@
+const spacing = {
+  tiny: 4,
+  smaller: 8,
+  medium: 12,
+  mediumPlus: 16,
+  large: 24,
+  huge: 32,
+  hugePlus: 48,
+};
+
+export default spacing;
